552d0bd9459288ef8b96961bab79b6c2c978e8ae
Fairy_Addict <nyashaperrry2005@yahoo.com>
2021-01-20 16:28:32 -0500

    commit tree 6988416fba10e1d627f71d45161fe3296d5abca9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
 class Dog
-    def initialize(name, breed = "Mutt")
-      @name = name
-      @breed = breed
-    end
+
+  def initialize(name, breed = "Mutt")
+    @name = name 
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end 
+
+  def breed
+    @breed
+  end
   end
